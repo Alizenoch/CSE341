45a7dc6fbae9d04b497343cb1bbcefd34a6065b6
Alizenoch <ahopa@byupathway.edu>
2026-03-18 10:36:11 +1000

    Added Swagger

---
+++ .history/server_20260318102641.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./db/connection');
+const contactsRoutes = require('./routes/contactsRoutes');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+
+
+// Routes
+app.use('/contacts', contactsRoutes);
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Welcome to the CSE341 Contacts API');
+});
+
+
+// Connect DB and start server
+connectDB();
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
---
+++ .history/server_20260318102913.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./db/connection');
+const contactsRoutes = require('./routes/contactsRoutes');
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+
+
+
+// Routes
+app.use('/contacts', contactsRoutes);
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Welcome to the CSE341 Contacts API');
+});
+
+
+// Connect DB and start server
+connectDB();
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
